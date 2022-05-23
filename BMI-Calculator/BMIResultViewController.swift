5e8f869784e0de37d9b6f4af56d9a21db0b125c1
yw22 <youngwook.dev@gmail.com>
2022-05-23 18:24:58 +0900

    init : "초기 설정"

---
+++ BMI-Calculator/BMIResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BMIResultViewController.swift
+//  BMI-Calculator
+//
+//  Created by 김영욱 on 2022/05/23.
+//
+
+import UIKit
+
+class BMIResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
