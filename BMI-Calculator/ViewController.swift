84c61e1658be120884788810627c5a43be512911
yw22 <youngwook.dev@gmail.com>
2022-05-23 20:46:35 +0900

    Feat : "기능 개선"
    number pad 에 Done 버튼 추가

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var heightTextField: UITextField!
     
     @IBOutlet weak var weightTextField: UITextField!
@@ -21,11 +21,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
         heightTextField.delegate = self
         weightTextField.delegate = self
         
-        
         setting()
+        setupToolbar()
         
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
-                view.addGestureRecognizer(tapGesture)
+        view.addGestureRecognizer(tapGesture)
         
     }
     
@@ -49,20 +49,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
         resultButton.layer.masksToBounds  = true
         
     }
-
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         // get the current text, or use an empty string if that failed
         let currentText = textField.text ?? ""
-
+        
         // attempt to read the range they are trying to change, or exit if we can't
         guard let stringRange = Range(range, in: currentText) else { return false }
-
+        
         // add their new text to the existing text
         let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-
+        
         // make sure the result is under 16 characters
         return updatedText.count <= 3
     }
+    
+    func setupToolbar(){
+        //Create a toolbar
+        let bar = UIToolbar()
+        //Create a done button with an action to trigger our function to dismiss the keyboard
+        let doneBtn = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissMyKeyboard))
+        //Create a felxible space item so that we can add it around in toolbar to position our done button
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        //Add the created button items in the toobar
+        bar.items = [flexSpace, flexSpace, doneBtn]
+        bar.sizeToFit()
+        //Add the toolbar to our textfield
+        heightTextField.inputAccessoryView = bar
+        weightTextField.inputAccessoryView = bar
+    }
+    
     
     @IBAction func resultButtonTapped(_ sender: UIButton) {
         guard let BMIResult = storyboard?.instantiateViewController(withIdentifier: "BMIResult") as? BMIResultViewController else { return }
@@ -72,6 +88,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
         present(BMIResult, animated: true, completion: nil)
         weightTextField.text = nil
         heightTextField.text = nil
+    }
+    
+    @objc func dismissMyKeyboard(){
+        view.endEditing(true)
     }
     
 }
