06552bb65e15c7b885531afb9260f5b6d96361fb
yw22 <youngwook.dev@gmail.com>
2022-05-24 18:42:29 +0900

    Feat : "기능 개선"
    BMI 단계 추가, 코드 개선

--- BMI-Calculator/BMIResultViewController.swift
+++ BMI-Calculator/BMIResultViewController.swift
@@ -22,29 +22,39 @@ class BMIResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        resultLabel.text = String(format: "%.2f", calculator(weight, height))
-        
         setting()
+        
+        resultLabel.text = String(format: "%.2f", calculator(height, weight))
     }
     
-    func calculator(_ : String?, _ : String?) -> Double {
+    func calculator(_ height: String?, _ weight: String?) -> Double {
         guard let weight = weight, let height = height else { return 0 }
-        let x = Double(Int(height)!) / 100
-        resultString.text = resultStr(Double(Int(weight)!) / (x * x))
+        guard let w = Double(weight), let h = Double(height) else { return 0 }
         
-        return Double(Int(weight)!) / (x * x)
+        resultString.text = resultStr( w / (h * h) * 10000 )
+        
+        return w / (h * h) * 10000
     }
     
     func resultStr(_ num : Double) -> String? {
         switch num{
-        case ...20:
+        case ...18.5:
+            resultLabel.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
             return "\"저체중\"입니다."
-        case 21...24:
+        case 18.6 ... 22.9:
+            resultLabel.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
             return "\"정상\"입니다."
-        case 25...29:
+        case 23.0 ... 24.9:
+            resultLabel.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
             return "\"과체중\"입니다"
-        case 30...:
-            return "\"비만\"입니다"
+        case 25.0...29.9:
+            resultLabel.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+            resultLabel.textColor = .white
+            return "\"중도 비만\"입니다"
+        case 30.0...:
+            resultLabel.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            resultLabel.textColor = .white
+            return "\"고도 비만\"입니다"
         default:
             return nil
         }
@@ -64,7 +74,7 @@ class BMIResultViewController: UIViewController {
     
 
     @IBAction func restartButton(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
     
 }
--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     
+    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var heightTextField: UITextField!
     
     @IBOutlet weak var weightTextField: UITextField!
@@ -24,9 +25,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         setting()
         setupToolbar()
         
-        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
-        view.addGestureRecognizer(tapGesture)
-        
     }
     
     
@@ -34,14 +32,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
         heightTextField.layer.cornerRadius = 12
         heightTextField.layer.masksToBounds  = true
         heightTextField.placeholder = "cm단위로 입력해주세요"
-        heightTextField.keyboardType = .numberPad
+        heightTextField.keyboardType = .default
         
         
         
         weightTextField.layer.cornerRadius = 12
         weightTextField.layer.masksToBounds  = true
         weightTextField.placeholder = "kg단위로 입력해주세요"
-        weightTextField.keyboardType = .numberPad
+        weightTextField.keyboardType = .default
         
         
         
@@ -50,25 +48,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        // get the current text, or use an empty string if that failed
-        let currentText = textField.text ?? ""
-        
-        // attempt to read the range they are trying to change, or exit if we can't
-        guard let stringRange = Range(range, in: currentText) else { return false }
-        
-        // add their new text to the existing text
-        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-        
-        // make sure the result is under 16 characters
-        return updatedText.count <= 3
-    }
+    
     
     func setupToolbar(){
         //Create a toolbar
         let bar = UIToolbar()
         //Create a done button with an action to trigger our function to dismiss the keyboard
-        let doneBtn = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissMyKeyboard))
+        let doneBtn = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(dismissMyKeyboard))
         //Create a felxible space item so that we can add it around in toolbar to position our done button
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         //Add the created button items in the toobar
@@ -85,13 +71,51 @@ class ViewController: UIViewController, UITextFieldDelegate {
         BMIResult.modalPresentationStyle = .fullScreen
         BMIResult.weight = weightTextField.text
         BMIResult.height = heightTextField.text
-        present(BMIResult, animated: true, completion: nil)
-        weightTextField.text = nil
-        heightTextField.text = nil
+        if heightTextField.text == "" || weightTextField.text == ""{
+            mainLabel.text = "키와 몸무게를 입력해주세요"
+            mainLabel.textColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+        } else {
+            present(BMIResult, animated: true, completion: nil)
+            weightTextField.text = nil
+            heightTextField.text = nil
+        }
     }
     
     @objc func dismissMyKeyboard(){
         view.endEditing(true)
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // get the current text, or use an empty string if that failed
+        let currentText = textField.text ?? ""
+        
+        // attempt to read the range they are trying to change, or exit if we can't
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        
+        // add their new text to the existing text
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        // make sure the result is under 16 characters
+        return updatedText.count <= 3
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if heightTextField.text != "", weightTextField.text != "" {
+            weightTextField.resignFirstResponder()
+            return true
+        } else if heightTextField.text != "" {
+            weightTextField.becomeFirstResponder()
+            return true
+        }
+        return false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
